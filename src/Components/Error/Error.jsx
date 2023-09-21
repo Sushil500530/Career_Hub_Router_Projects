6ec3d12fdd2cfbeb4cc1f610e4febf5e783f1006
Sushil97 <sushil500530@gmail.com>
2023-09-21 18:45:15 +0600

    project set up with tailwindcss

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,11 @@
+
+const Error = () => {
+    return (
+        <div className="text-center mt-20">
+            <h2 className="text-4xl">Ooopps <span className="text-red-500 bold">!!!</span></h2>
+            <p className="text-2xl font-semibold">Page Not Found!</p>
+        </div>
+    );
+};
+
+export default Error;
